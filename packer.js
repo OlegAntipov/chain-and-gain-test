0d7993bb0e7d76192c1fddbb37664fa623bae0bf
Oleg Antipov <anegmetex@gmail.com>
2026-02-12 15:50:56 +0300

    Add files via upload

--- packer.js
+++ packer.js
@@ -79850,10 +79850,10 @@ var PlatformPlayDeck = class extends PlatformBase {
     this.playButtonIsPressed = false;
   }
   async init() {
-    AnLog.info(`build 002`);
-    if (window === window.parent)
+    AnLog.info(`bld003`);
+    if (window === window.parent.window)
       throw new Error("[PlayDeck SDK] \u043D\u0435 \u0437\u0430\u0433\u0440\u0443\u0436\u0435\u043D, \u043F\u0440\u043E\u0438\u0437\u043E\u0448\u043B\u0430 \u043E\u0448\u0438\u0431\u043A\u0430!");
-    this.wrapper = window.parent;
+    this.wrapper = window.parent.window;
     const connected = await this.callMethod("getPlaydeckState", null, true);
     if (!connected)
       throw new Error("[PlayDeck SDK] \u043E\u0432\u0435\u0440\u043B\u0435\u0439 \u043D\u0435 \u0438\u043D\u0438\u0446\u0438\u0430\u043B\u0438\u0437\u0438\u0440\u043E\u0432\u0430\u043D, \u043F\u0440\u043E\u0438\u0437\u043E\u0448\u043B\u0430 \u043E\u0448\u0438\u0431\u043A\u0430!");
